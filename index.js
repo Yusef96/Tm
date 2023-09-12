44d0578737496d29d0a6a84b3f7aa6ec8c763c09
Yusef96 <yosef.yh221@gmail.com>
2023-09-12 17:58:37 +0300

    sec commit

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 $(document).ready(() => {
-    $(document).on('click', ".title", function(e){
+  $(document).on('click', ".title", function (e) {
 
 
-        const isActive = $(e.currentTarget).next().hasClass("active");
+    const isActive = $(e.currentTarget).next().hasClass("active");
 
-        $(".desc.active").removeClass("active");
-        $(".title i.active").removeClass("active");
-        if (!isActive) {
-            $(e.currentTarget).next().toggleClass("active");
-            $(e.currentTarget).find("i").toggleClass("active");
-        }
-    })
+    $(".desc.active").removeClass("active");
+    $(".title i.active").removeClass("active");
+    if (!isActive) {
+      $(e.currentTarget).next().toggleClass("active");
+      $(e.currentTarget).find("i").toggleClass("active");
+    }
+  })
 });
 
-$(document).ready(function() {
-    $(".open-sec").click(function() {
-      $(".out").toggle(500);
-    });
-    $(".open-right").click(function() {
-      $(".right-sec").toggle(500);
-    });
-
+$(document).ready(function () {
+  $(".open-sec").click(function () {
+    $(".out").toggle(500);
   });
+  $(".open-right").click(function () {
+    $(".right-sec").toggle(500);
+  });
+
+  $('.home-mob').click(function () {
+    $('.right-mob').hide(350);
+    $('.mid-section').show();
+  });
+  $('.bvb').click(function () {
+    $('.mid-section').hide(350);
+    $('.right-mob').show();
+  });
+  ;
+
+});
